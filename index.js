6ed1b99712f5de4ca356d051c8983840b4ce02ac
Vinh Do <vinhdo07@gmail.com>
2019-11-22 04:07:28 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,118 @@
+/* 
+  1 frame = up to 2 throws
+  each number represents a throw
+  10 chances of 2 throws
+
+
+
+  on last frame:
+  spare on 10th = 1 more ball
+  strike on 10th = 2 more balls
+
+
+
+  compute score at the end of game
+  if strike, next throw will be affected
+*/
+// sample input - 3, 4, 8, 10
+
+/*
+  Clean-up: {
+    round-1: 50min,
+  }
+
+  Research: {
+    round-1: 15min
+  }
+*/
+
+
+let frames = 1
+let total = 0
+let strike = false
+let spare = false
+let firstThrow = false
+
+function score(num) {
+  if (frames === 1) {
+    firstFrame(num)
+    return
+  }
+
+
+  if (firstThrow && strike) {
+    total += 2 * (firstThrow + num)
+    strike = false
+    frames++
+  } else if (firstThrow) {
+    addThrowsToTotal(num)
+  } else {
+    setFirstThrow(num)
+  }
+
+  console.log("current score is: ", total, "\ncurrent frame is: ", frames)
+
+
+  if (frames === 11) {
+    if (firstThrow && strike) {
+      total += 2 * (firstThrow + num)
+    } else if (strike) {
+      setFirstThrow(num)
+    } else {
+      let finalScore = total + num
+      total = 0
+      strike = false
+      firstThrow = false
+      frames = 1
+      console.log("FINAL: ", finalScore)
+      return finalScore
+    }
+  }
+}
+
+/* Handles the first frame since strike here would mean just 10pts. */
+function firstFrame(num) {
+  if (num === 10) {
+    strike = true
+    total += num
+    frames++
+  } else if (firstThrow) {
+    addThrowsToTotal(num)
+  } else {
+    setFirstThrow(num)
+  }
+}
+
+
+function setFirstThrow(num) {
+  firstThrow = num
+}
+
+function addThrowsToTotal(num) {
+  let subtotal = (firstThrow + num)
+  if (subtotal === 10) {
+    spare = true
+  }
+  firstThrow = false
+  frames++
+}
+
+
+
+
+score(10)
+score(2)
+score(3)
+
+frames = 10
+
+score(10)
+score(6)
+score(4)
+
+
+score(2)
+
+
+
+
